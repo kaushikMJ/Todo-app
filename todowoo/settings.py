bb71f4b593a1123115ae2947a8afbec928177f26
Kaushik Mahajan <voicekaushik@gmail.com>
2022-06-30 06:31:59 +0530

    third push

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 import django_heroku
 import dj_database_url
-import os 
+
 
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
